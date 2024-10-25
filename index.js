c8b1c66d10105467cc1c876ac60e9ac09a023d26
Dmytro Sokolovskyi <dn24062001@gmail.com>
2024-10-25 21:23:00 +0300

    added express with port

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+require('dotenv').config();
+
 const bot = require('./bot/bot');
+
+const PORT = process.env.PORT || 3000;
+
+const express = require('express');
+
+const app = express();
 
 // Обробляється помилка при опитуванні бота
 bot.on('polling_error', console.log);
 
+app.listen(PORT, () => {
+  console.log(`Бот запущений на порту ${PORT}`);
+});
