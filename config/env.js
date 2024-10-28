d070e69c2f066c1501c9da76947702aa163f8c0f
Dmytro Sokolovskyi <dn24062001@gmail.com>
2024-10-28 18:59:01 +0200

    ui url changed

--- config/env.js
+++ config/env.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 module.exports = {
   // Експорт токена бота з змінної середовища, URL веб-додатку та URL API для взаємодії з бекендом
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
-  // WEB_APP_URL: 'https://dmytrosokolovsky.github.io/react_diplom/#/',
-  WEB_APP_URL: 'https://diplom-ui-ssr-pzyd.vercel.app/',
+  WEB_APP_URL: 'https://dmytrosokolovsky.github.io/react_diplom/#/',
+  // WEB_APP_URL: 'https://diplom-ui-ssr-pzyd.vercel.app/',
   API_URL: "https://api-diplom-eupy.onrender.com/api/",
 };
